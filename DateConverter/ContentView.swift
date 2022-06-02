f5dd58b69dcd43246a62f5cb221653aedd681b88
Lucas Chae <hi@lucaschae.com>
2022-06-02 13:33:36 +0900

    Initial Commit

---
+++ DateConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DateConverter
+//
+//  Created by Lucas Chae on 6/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DateConverter/DateConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateConverterApp.swift
+//  DateConverter
+//
+//  Created by Lucas Chae on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DateConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
